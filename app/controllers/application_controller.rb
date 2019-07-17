7a41c79b37e32a17e786a0aae73c41671a35e5d9
github username <github email address>
2019-07-17 18:51:10 -0400

    First Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 require './config/environment'
+require 'sinatra'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,7 +10,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    @artist = Artist.all
     erb :welcome
   end
 
+  get "/new" do
+    erb :new
+  end
+
+  post "/" do
+    @artist = Artist.create(params[:artist])
+    redirect to "/#{@artist.id}"
+  end
+
+  get "/:id" do
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
 end
